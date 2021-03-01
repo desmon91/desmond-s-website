e955d86094e9e6c87d363a7127575599e13e2b57
Desmond Kristian Siahaan <Desmond_Siahaan@BANPUINDO.co.id>
2021-03-01 13:38:54 +0800

    fix gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,11 +10,6 @@ module.exports = {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
-          // gatsby-remark-relative-images-v2 must
-          // go before gatsby-remark-images
-          {
-            resolve: `gatsby-remark-relative-images-v2`,
-          },
           {
             resolve: `gatsby-remark-images`,
             options: {
